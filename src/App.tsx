2c1a3d842eb48b2a3730f389aeb7ee82f569c545
Dragan Bijelic <bijelic.03.dragan@gmail.com>
2024-04-17 13:08:51 +0200

    Changed private route logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
-
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-      </Routes>
-      <PrivateRoute path='/' element={<HomePage />} />
-    </>
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/' element={<PrivateRoute />}>
+        <Route index element={<HomePage />} />
+        <Route path='profile' />
+      </Route>
+    </Routes>
   );
 };
 
