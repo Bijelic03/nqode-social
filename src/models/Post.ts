ecc1c605761b31498d29de08335664821f8290bf
Dragan Bijelic <83468499+Bijelic03@users.noreply.github.com>
2024-04-22 14:41:33 +0200

    N-10 Post form implementation (#8)
    Co-authored-by: Dragan Bijelic <bijelic.03.dragan@gmail.com>

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,7 +1,15 @@
 import { User } from './User';
 
 export interface Post {
+  id: number;
   author: User;
   text: string;
+  authorId: number;
   mediaPaths: string[];
+}
+
+export interface PostCreate {
+  text: string;
+  file?: File | null;
+  authorId: number;
 }
